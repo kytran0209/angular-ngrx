c28fa99e579e36eb9a029be72549d0b82cb1ed1f
Ky Tran <youarenotalone0209@gmail.com>
2020-12-08 21:34:38 -0500

    Tutorial 1

---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,29 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on
+} from '@ngrx/store';
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
+
+export interface AuthState {
+  user: User
+}
+
+export const initialAuthState: AuthState = {
+  user: undefined
+}
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => ({
+    user: action.user
+  })),
+  on(AuthActions.logout, (state, action) => ({
+    user: undefined
+  }))
+);
